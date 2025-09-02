ec049179ced1098acf1216c84ac02db91a6ac0fb
casper9474 <casper94749474@gmail.com>
2025-09-02 21:32:25 +0200

    fix: better foldtext formatting

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -23,6 +23,7 @@ vim.o.softtabstop = -1 -- Number of spaces inserted instead of a TAB character
 -- Folding options
 vim.o.foldlevel = 99        -- Open all fold by default
 vim.o.foldmethod = "indent" -- Better folding
+vim.opt.foldtext = "v:lua.custom_foldtext()"
 
 -- Diagnostics
 vim.diagnostic.config({
@@ -85,3 +86,41 @@ vim.api.nvim_create_autocmd("LspAttach", {
         vim.opt_local.updatetime = 500
     end,
 })
+
+-- Functions
+local function fold_virt_text(result, s, lnum, coloff)
+    if not coloff then
+        coloff = 0
+    end
+    local text = ""
+    local hl
+    for i = 1, #s do
+        local char = s:sub(i, i)
+        local hls = vim.treesitter.get_captures_at_pos(0, lnum, coloff + i - 1)
+        local _hl = hls[#hls]
+        if _hl then
+            local new_hl = "@" .. _hl.capture
+            if new_hl ~= hl then
+                table.insert(result, { text, hl })
+                text = ""
+                hl = nil
+            end
+            text = text .. char
+            hl = new_hl
+        else
+            text = text .. char
+        end
+    end
+    table.insert(result, { text, hl })
+end
+
+function _G.custom_foldtext()
+    local start = vim.fn.getline(vim.v.foldstart):gsub("\t", string.rep(" ", vim.o.tabstop))
+    local end_str = vim.fn.getline(vim.v.foldend)
+    local end_ = vim.trim(end_str)
+    local result = {}
+    fold_virt_text(result, start, vim.v.foldstart - 1)
+    table.insert(result, { " ... ", "Delimiter" })
+    fold_virt_text(result, end_, vim.v.foldend - 1, #(end_str:match("^(%s+)") or ""))
+    return result
+end
