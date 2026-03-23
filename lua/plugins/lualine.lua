2910a8ad1c51f635d4860f2538aff8cd711017b4
casper9474 <casper94749474@gmail.com>
2026-03-23 23:30:16 +0100

    chagne colorscheme and lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,8 +1,17 @@
 return {
     "nvim-lualine/lualine.nvim",
     opts = {
+        options = {
+            component_separators = { left = "", right = "" },
+            section_separators = { left = "", right = "" }
+        },
         tabline = {
             lualine_a = { "buffers" },
         },
+        sections = {
+            lualine_x = { "encoding", "filetype" },
+            lualine_y = { "progress" },
+            lualine_z = { "location" }
+        }
     }
 }
