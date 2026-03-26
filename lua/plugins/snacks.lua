680d0f83d836a5008f7f52822c42cd62cebe517e
casper9474 <casper94749474@gmail.com>
2026-03-26 17:29:31 +0100

    remove fold markers

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,7 +5,7 @@ return {
     ---@module "snacks"
     ---@type snacks.Config
     opts = {
-        statuscolumn = { enabled = true, folds = { open = true, git_hl = true, } },
+        statuscolumn = { enabled = true },
         lazygit = { enabled = true },
         explorer = { enabled = true, replace_netrw = true },
         picker = { enable = true },
